1bb6eb89ae63703909ec323d4a704aff84f7e214
Cwz-Eikoh <cwz_eikoh@163.com>
2023-04-13 18:02:39 +0800

    实现了时钟的显示以及设置倍率、加速、暂停功能

--- campus_local/mainwindow.cpp
+++ campus_local/mainwindow.cpp
@@ -1,8 +1,14 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include "timer.h"
 #include <QMessageBox>
 #include <QLabel>
 #include <QCloseEvent>
+#include <QTimer>
+#include <QDialog>
+#include <QInputDialog>
+#include <QLineEdit>
+#include <QIntValidator>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -10,13 +16,28 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    //在状态栏显示信息
+    //初始化时间
+    tim = new timer();
+    //初始化并美化时间标签
+    timlabl = new QLabel(this);//显示时间的标签
+    timlabl->setFrameShape(QLabel::WinPanel);
+    timlabl->setFrameShadow(QLabel::Sunken);
+    timlabl->setText(tim->toString("yyyy年MM月dd日 hh:mm"));
+    ui->statusbar->addWidget(timlabl);
+    //设置一个计时器
+    QTimer *UpgraCount = new QTimer(this);//用于设置刷新时间
+    UpgraCount->start(1000);//每秒刷新一次
+    //连接倒计时与时间数据更新
+    connect(UpgraCount, SIGNAL(timeout()), this, SLOT(timShowNewest()));
+
+    //在状态栏显示项目地址信息
     QLabel *perm = new QLabel(this);
     perm->setFrameStyle(QFrame::Box|QFrame::Sunken);
     perm->setText(tr("<a href=\"https://live.bilibili.com/22603245?live_from=85001&spm_id_from=444.41.live_users.item.click\">项目源码地址<a>"));
     perm->setTextFormat(Qt::RichText);
     perm->setOpenExternalLinks(true);
     ui->statusbar->addPermanentWidget(perm);
+
 }
 
 MainWindow::~MainWindow()
@@ -49,3 +70,30 @@ void MainWindow::ReiceiveUser(const User * data)
 {
     per = data;
 }
+
+void MainWindow::timShowNewest()
+{
+    tim->update();
+    timlabl->setText(tim->toString("yyyy年MM月dd日 hh:mm"));
+}
+
+void MainWindow::on_actionBeginTim_B_triggered()
+{
+    tim->begin();
+}
+
+
+void MainWindow::on_actionPauseTim_P_triggered()
+{
+    tim->puase();
+}
+
+
+void MainWindow::on_actionChgTimRa_T_triggered()
+{
+    bool ok;
+    int raio = QInputDialog::getInt(this, tr("调整时间倍率"), tr("请设置\n   程序1秒钟/现实1分钟\n的比值"), 1, 0, 60, 1, &ok);
+    if(ok)
+        tim->set_ratio(raio*60);
+}
+
--- campus_local/mainwindow.h
+++ campus_local/mainwindow.h
@@ -4,6 +4,9 @@
 #include <QMainWindow>
 #include"person.h"
 
+//前置定义
+class timer;
+class QLabel;
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -21,9 +24,18 @@ private:
     Ui::MainWindow *ui;
     const User* per;//存放登录的用户
     void closeEvent(QCloseEvent *event);//处理关闭窗口事件
+    timer* tim;//保存时间
+    QLabel* timlabl;//显示时间的标签
 private slots:
     void on_action_S_triggered();//切换用户的槽
     void ReiceiveUser(const User*);
+    void timShowNewest();//用于更新时间信息的槽
+    void on_actionBeginTim_B_triggered();
+
+    void on_actionPauseTim_P_triggered();
+
+    void on_actionChgTimRa_T_triggered();
+
 signals:
     void LogOut();
 };
